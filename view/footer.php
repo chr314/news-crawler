449ae13f7f1e60a2f492588e8695ee003a7ecaf3
Christoforos Aslanov <christoforos@christoforos.io>
2019-06-04 03:47:25 +0300

    added custom scripts,styles in footer, footer update

--- view/footer.php
+++ view/footer.php
@@ -1,18 +1,33 @@
-
-<footer class="bd-footer text-muted">
-    <div class="container-fluid p-3 p-md-5">
-        <ul class="bd-footer-links">
-            <li><a href="https://github.com/chr314/news-crawler">GitHub</a></li>
-            <li><a href="https://twitter.com/christoforos_io">Twitter</a></li>
-            <li><a href="#">About</a></li>
-        </ul>
-        <p></p>
-    </div>
+<footer class="blog-footer">
+    <p>Developed <a href="https://twitter.com/christoforos_io">@christoforos_io</a></p>
+    <p>
+        <a href="#">Back to top</a>
+    </p>
 </footer>
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+
+<?php
+if (!empty($scripts) && is_array($scripts)) {
+    foreach ($scripts as $script) {
+        echo '<script src="' . $script . '"></script>';
+    }
+}
+
+if (!empty($styles) && is_array($styles)) {
+    foreach ($styles as $style) {
+        echo '<link href="' . $style . '" rel="stylesheet" type="text/css">';
+    }
+}
+?>
 </body>
 </html>
