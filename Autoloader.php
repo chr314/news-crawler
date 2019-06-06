e4e6184c840b5a25e5efa91a321c7cdff7455135
Christoforos Aslanov <christoforos@christoforos.io>
2019-06-06 17:34:39 +0300

    support for sub dirs in loader & router

--- Autoloader.php
+++ Autoloader.php
@@ -10,10 +10,19 @@ class Autoloader
         if (count($name_parts) > 1) {
             $found_dir = array_search($name_parts[0], $dist_dirs);
 
-            $class_path = __DIR__ . "/" . $found_dir . "/" . strtolower(implode('_', array_slice($name_parts, 1))) . ".php";
+            $file_path_pieces = array_slice($name_parts, 1);
 
-            if (is_file($class_path)) {
-                include_once $class_path;
+            for ($x = 0; $x < count($file_path_pieces); $x++) {
+                $file_name = implode("_", array_slice($file_path_pieces, $x));
+                $path = __DIR__ . "/" . $found_dir . "/" . implode("/", array_map('strtolower', array_slice($file_path_pieces, 0, $x)));
+
+                $file_arr = glob($path . '*.php', GLOB_NOSORT);
+                $file_lcase = strtolower($file_name) . ".php";
+                foreach ($file_arr as $file) {
+                    if (strtolower(basename($file)) == $file_lcase) {
+                        include_once $file;
+                    }
+                }
             }
         } elseif (is_file(__DIR__ . "/" . $className . ".php")) {
             include_once __DIR__ . "/" . $className . ".php";
--- index.php
+++ index.php
@@ -6,20 +6,20 @@ if (!empty($_GET["route"]) && preg_match_all("/([\w-]+)\/?/", $_GET["route"], $r
 
     if (isset($route_pieces[1]) && is_array($route_pieces[1]) && count($route_pieces[1]) > 1) {
         $route_pieces = $route_pieces[1];
-        $class_pieces = array_slice($route_pieces, -2);
+        $class_pieces = array_slice($route_pieces, 0, -1);
 
-        $file_name = $class_pieces[0];
-        $class_name = "Controller_" . $file_name;
-        $func_name = $class_pieces[1];
+        $class_name = "Controller";
+        foreach ($class_pieces as $class_piece) {
+            $class_name .= "_" . ucfirst(strtolower($class_piece));
+        }
 
-        if (!empty($file_name) && !empty($func_name) && file_exists(__DIR__ . "/controller/" . implode('/', array_slice($route_pieces, 0, -1)) . ".php")) {
+        $func_name = array_slice($route_pieces,-1)[0];
 
-            if (class_exists($class_name)) {
-                $route_class = new $class_name();
-                if (is_callable(array($route_class, $func_name))) {
-                    $route_class->$func_name();
-                    exit;
-                }
+        if (!empty($func_name) && class_exists($class_name)) {
+            $route_class = new $class_name();
+            if (is_callable(array($route_class, $func_name))) {
+                $route_class->$func_name();
+                exit;
             }
         }
     }
